a35e06574b31dffd0d50ea0ad571eda7cec8047d
Vivian Dang <viv_yd@Vivians-MacBook-Pro.local>
2018-03-21 13:21:24 +1100

    used webargs to parse parameters, function now also return JSON, and added error handling for invalid inputs

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, abort
+from webargs import fields, validate
+from webargs.flaskparser import use_kwargs, parser 
 
 app = Flask(__name__)
 api = Api(app)
 
 class Company(Resource):
-    def get(self, name):
+    args = {
+        'start_time': fields.DateTime(format="%Y-%m-%dT%H:%M:%S.%fZ", required=True),
+        'end_time': fields.DateTime(format="%Y-%m-%dT%H:%M:%S.%fZ", required=True),
+    }
+    @use_kwargs(args)
+    def get(self, name, start_time, end_time):
         if name == "Viv":
-            return name, 200
-        return "Never heard of this company", 404
+            return {"name": name, "start": str(start_time), "end": str(end_time)}, 200
+        return "dunno what you talking about", 404 
 
 @app.route('/')
 def index():
     return "Hello, World!"
 
 
+@parser.error_handler
+def handle_error(err):
+    abort(422, errors=err.messages)
 
 if __name__ == '__main__':
     api.add_resource(Company, "/company/<string:name>")
