8136b7365c00489a9b51abe4f734eea89f38b99d
dbunadi <dbunadi@gmail.com>
2018-03-24 18:35:41 +1100

    added steps for get company

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3
-from flask import Flask
+#!flask/bin/python
+from flask import Flask, request
 from flask_restful import Api, Resource, abort
 from webargs import fields, validate
 from webargs.flaskparser import use_kwargs, parser 
@@ -8,18 +8,29 @@ app = Flask(__name__)
 api = Api(app)
 
 class Company(Resource):
+    
     args = {
         'start_time': fields.DateTime(format="%Y-%m-%dT%H:%M:%S.%fZ", required=True),
         'end_time': fields.DateTime(format="%Y-%m-%dT%H:%M:%S.%fZ", required=True),
     }
+    
     @use_kwargs(args)
     def get(self, name, start_time, end_time):
+        print name, start_time, end_time
+        
+        # Check to see if name is an instrument id
+            # if yes then convert
+        
+        # Search for the company's facebook page and get it's page id
+
+        # Get page stats
+
+        # Get page posts
+
+        # Make JSON result
+
+
         return {"name": name, "start": str(start_time), "end": str(end_time)}, 200
-
-@app.route('/')
-def index():
-    return "TESTESTEST QT314!"
-
 
 @parser.error_handler
 def handle_error(err):
