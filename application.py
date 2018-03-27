b0751f4e16594aac0f41427d17351d6364704160
Chris Bui <christinetvbui@gmail.com>
2018-03-28 01:22:10 +1100

    returns json output using jsonify

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
 #usr/bin/env python3
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api, Resource, abort
 from webargs import fields, validate
 from webargs.flaskparser import use_kwargs, parser 
-import json, re
+import re
 
 # import fncs from files
 from other import get_asx_list
@@ -14,50 +14,48 @@ api = Api(app)
 
 class Company(Resource):
 	asx_dict = get_asx_list()
+	print asx_dict.keys()
 
 	# returns facebook page id
 	def getFacebookID(self, name):
 		# name can be provided as ASX ticker code (WOW.ASX) OR company name (Woolworths)	
 		# if ticker code (regex ***.ASX: look through dict
 		company_name = name		
-		match = re.match(r'([A-Za-z]{3})\.ASX', name).group(1)
-		if match is not None and match in asx_dict:
-			#if match in asx_dict
-			company_name = asx_dict[match]
-		else:
-			pass #if not a match then must be a searchable company name already	
-
+		try:
+			match = re.match(r'([A-Za-z]{3})\.ASX', name).group(1)
+			if match is not None and match in self.asx_dict:
+				#if match in asx_dict
+				company_name = self.asx_dict[match]
+				# we should also edit out LIMITED stuff because that never shows up on search fb api
+			else:
+				pass #if not a match then must be a searchable company name already	
+		except:
+			pass	
+		return company_name
 					
 			
 		# use https://graph.facebook.com/v2.12/search?q=<COMPANYNAME>&type=page
 
-    args = {
+	args = {
         'start_time': fields.DateTime(format="%Y-%m-%dT%H:%M:%S.%fZ", required=True),
         'end_time': fields.DateTime(format="%Y-%m-%dT%H:%M:%S.%fZ", required=True),
-		'CompanyID': fields.Str(),	# e.g. 'WOW.AX' or 'Woolworths' 
+		'CompanyID': fields.Str(required=False),	# e.g. 'WOW.AX' or 'Woolworths' 
 										# (format is identified in getFacebookID)
 		#'CompanyIDs': fields.Str(),	# e.g. 'Woolworths'
-		'stats': fields.DelimitedList(fields.Str(), required=True),
+		#'stats': fields.DelimitedList(fields.Str(), required=True),
 				# example usage: "/?stats=id,name,website,description"
 				# stats can include id, name, website, description, category, fan_count, post_type, post_message, post_created_time, post_like_count, post_comment_count
 			}
-    @use_kwargs(args)
-    def get(self, start_time, end_time, CompanyID):
-		page_id = getFacebookID(CompanyID);
-        #return 		{"start": str(start_time), #TESTING PURPOSES ONLY
-		#			 "end": str(end_time), #TESTING PURPOSES ONLY
-		#			 "PageId"= str(page_id)},
-		#			 200
-		return json.dumps(	{"Facebook Statistic Data":[
-								{
-									"Start": start_time,
-									"End": end_time, 
-									"PageId": page_id,
-								
-								}
-							]
-
-							),200
+	@use_kwargs(args)
+	# GET must return json output with: PageId, InstrumentIDs, CompanyNames, PageName, Website, Description, Category, fan_count, posts[id, type, message, created_time, like_count, comment_count]
+	def get(self, name, start_time, end_time, CompanyID):
+		page_id = self.getFacebookID(str(CompanyID));
+		print page_id;
+		return jsonify(
+			Start=(str(start_time)), 	# TESTING ONLY
+			End=str(end_time),			# TESTING ONLY
+			PageId=page_id
+		)
 
 @app.route('/')
 def index():
