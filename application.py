6d258899c0d94afb0d3efc569de5107e2d29b3e6
Chris Bui <christinetvbui@gmail.com>
2018-03-29 18:12:31 +1100

    user can input interested statistics -- COMPLETE

--- application.py
+++ application.py
@@ -15,31 +15,7 @@ api = Api(app)
 
 class Company(Resource):
     asx_dict = get_asx_list()
-    print asx_dict.keys()[0] # testing only
-
-    # def getFacebookID(self, name):
-    #     # name can be provided as ASX ticker code (WOW.ASX) OR company name (Woolworths)    
-    #     # if ticker code (regex ***.ASX): look through dict
-    #     company_name = name
-    #     try:
-    #         #if name provided is an instrument id
-    #         match = re.match(r'([A-Za-z]{3})\.ASX', name).group(1)
-    #         if match is not None and match in self.asx_dict:
-    #             company_name = self.asx_dict[match]
-    #             company_name = re.sub('\sLIMITED','',company_name)
-    #             instrument_id = name
-    #         else:
-    #             print("Company not found")
-    #             ### eRROR HANDLING REQUIRED
-    #     except:
-    #         pass
-
-    #     #TODO: if company name string provided, find instrument id
-    #     return company_name
-                    
-    # def createFields(self, stats):
-    #     page_stats = ''
-    #     post_stats = ''
+    # print asx_dict.keys()[0] # testing only
 
     args = {
         'start_time': fields.DateTime(format="%Y-%m-%dT%H:%M:%S.%fZ", required=True),
@@ -58,56 +34,39 @@ class Company(Resource):
             if stats is not None:
                 page_fields, post_fields = createFields(stats)
 
-            print "PAGE_FIELDS:", page_fields, "POST_FIELDS:", post_fields
             # Search for the company's facebook page and get it's page id
             page_search = requests.get("https://graph.facebook.com/v2.12/search?q=%s&type=page&fields=verification_status&access_token=%s" % (page_name, os.environ['FB_API_KEY'])).json()
+            #print page_search
 
             #Find the first blue verified result
             #TODO: Figure out how to validate that the page is the right one
             page_id = page_search['data'][0]['id']        
             for x in page_search['data']:
                 if x['verification_status'] == 'blue_verified':
-                    # print "FOUND BLUE VERIFIED"
                     page_id = x['id']
                     break;
 
             # Get page stats
-            # print page_fields, post_fields
-            page_stats = requests.get("https://graph.facebook.com/v2.11/%s?fields=name,website,about,category,fan_count&access_token=%s" % (page_id, os.environ['FB_API_KEY'])).json()  
-            # print page_stats
+            page_stats = requests.get("https://graph.facebook.com/v2.11/%s?fields=%s&access_token=%s" % (page_id, page_fields, os.environ['FB_API_KEY'])).json()  
             # Get page posts
+            page_posts = requests.get("https://graph.facebook.com/v2.11/%s/posts?fields=%s&access_token=%s" % (page_id, post_fields, os.environ['FB_API_KEY'])).json()['data'] 
 
-            #convert stats to request format
+            for post in range(len(page_posts)):
+                if 'post_id' in stats:
+                    page_posts[post]['post_id'] = page_posts[post].pop('id')
+                if 'post_type' in stats:
+                    page_posts[post]['post_type'] = page_posts[post].pop('type')
+                if 'post_like_count' in stats:
+                    page_posts[post]['post_like_count'] = page_posts[post]['likes']['summary']['total_count']
+                    page_posts[post].pop('likes')
+                if 'post_comment_count' in stats:
+                    page_posts[post]['post_comment_count'] = page_posts[post]['comments']['summary']['total_count']
+                    page_posts[post].pop('comments')
+                if 'post_message' in stats and 'message' in page_posts[post]:
+                         page_posts[post]['post_message'] = page_posts[post].pop('message')
+                if 'post_created_time' in stats:
+                        page_posts[post]['post_created_time'] = page_posts[post].pop('created_time')
 
-            # page_posts = requests.get("https://graph.facebook.com/v2.11/%s/posts?access_token=%s" % (page_id, os.environ['FB_API_KEY'])).json()['data'] 
-
-            # for post in range(len(page_posts)):
-            #     # print(page_posts[post])
-            #     temp_post = page_posts[post]
-            #     if temp_post['id'] is not None:
-            #         print temp_post['id']
-            
-            #     temp_post['post_id'] = temp_post.pop('id')
-            #     # if temp_post['message']:
-            #     if 'message' in temp_post:
-            #         temp_post['post_message'] = temp_post.pop('message')
-            #     temp_post['post_created_time'] = temp_post.pop('created_time')
-
-            #     post_type = requests.get("https://graph.facebook.com/v2.11/%s?fields=type&access_token=%s" % (page_posts[post]['post_id'], os.environ['FB_API_KEY'])).json()['type']
-            #     temp_post['post_type'] = post_type
-                
-            #     post_likes = requests.get("https://graph.facebook.com/v2.11/%s/likes?summary=true&access_token=%s" % (page_posts[post]['post_id'], os.environ['FB_API_KEY'])).json()['summary']['total_count']
-            #     temp_post['post_like_count'] = post_likes
-
-
-            #     post_comments = requests.get("https://graph.facebook.com/v2.11/%s/comments?summary=true&access_token=%s" % (page_posts[post]['post_id'], os.environ['FB_API_KEY'])).json()['summary']['total_count']
-            #     temp_post['post_comment_count'] = post_comments
-
-            #     page_posts[post] = temp_post
-                
-
-
-            # Make JSON result
             result = {}
             if 'id' in stats:
                 result['PageId'] = page_stats.pop('id')
@@ -116,17 +75,17 @@ class Company(Resource):
 
             if 'name' in stats:
                 result['PageName'] = page_stats.pop('name')
-            if 'website' in stats:
+            if 'website' in stats and 'website' in page_stats:
                 result['Website'] = page_stats.pop('website')
-            if 'description' in stats and 'about' in page_stats:
-                result['Description'] = page_stats.pop('about')
+            if 'description' in stats and 'description' in page_stats:
+                result['Description'] = page_stats.pop('description')
             if 'category' in stats:
                 result['Category'] = page_stats.pop('category')
             if 'fan_count' in stats:
                 result['fan_count'] = page_stats.pop('fan_count')
 
 
-            # result['posts'] = page_posts
+            result['posts'] = page_posts
             
             response = {}
             response['FacebookStatisticData'] = result
