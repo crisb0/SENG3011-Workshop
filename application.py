d05f15dc3a6d3f9f2dc2cc71972a21dcbab53e5b
Chris Bui <christinetvbui@gmail.com>
2018-03-29 11:40:35 +1100

    changes to ease pain of merging

--- application.py
+++ application.py
@@ -16,34 +16,27 @@ class Company(Resource):
 	asx_dict = get_asx_list()
 	print asx_dict.keys()
 
-	################################
-	#### GET FACEBOOK ID		####
-	#### input: query string	####
-	#### return: fb page ID		####
-	################################
 	def getFacebookID(self, name):
 		# name can be provided as ASX ticker code (WOW.ASX) OR company name (Woolworths)	
-		# if ticker code (regex ***.ASX: look through dict
+		# if ticker code (regex ***.ASX): look through dict
 		company_name = name		
 		try:
 			match = re.match(r'([A-Za-z]{3})\.ASX', name).group(1)
 			if match is not None and match in self.asx_dict:
 				company_name = self.asx_dict[match]
-				print company_name
 				company_name = re.sub('\sLIMITED','',company_name) # remove LIMITED bc it doesn't return results usually
-				print company_name
 		except:
-			pass	
+			pass
 		return company_name
 					
 			
-		# use https://graph.facebook.com/v2.12/search?q=<COMPANYNAME>&type=page
 
 	args = {
         'start_time': fields.DateTime(format="%Y-%m-%dT%H:%M:%S.%fZ", required=True),
         'end_time': fields.DateTime(format="%Y-%m-%dT%H:%M:%S.%fZ", required=True),
 		'stats': fields.DelimitedList(fields.Str(), required=False),
 				# example usage: "/?stats=id,name,website,description"
+				# "/?stats=all" will return ALL POSSIBLE RESULTS
 				# stats can include id, name, website, description, category, fan_count, post_type, post_message, post_created_time, post_like_count, post_comment_count
 			}
 	@use_kwargs(args)
@@ -54,31 +47,17 @@ class Company(Resource):
 	####################################
 	# GET must return json output with: PageId, InstrumentIDs, CompanyNames, PageName, Website, Description, Category, fan_count, posts[id, type, message, created_time, like_count, comment_count]
 	def get(self, name, start_time, end_time, stats):
-		page_id = self.getFacebookID(str(name));
+		page_name = self.getFacebookID(str(name));
 		print stats;
 		for x in stats:
 			print x;
 			# SEARCH FOR INTERESTED STATISTICS HERE
-		print page_id;
-		return {"name": name,
-				"start":str(start_time),
-				"end": str(end_time),
-				"PageID": page_id,
-				"InstrumentIDs":'',
-				"CompanyNames":'',
-				"PageName":'',
-				"Website":'',
-				"Description":'',
-				"Category":'',
-				"fan_count":'',
-				"posts":{
-					"hello":'',
-					
-				}
+		print page_name;
+		return "json obj";	
 
 
 
-		}
+		
 		
 
 @app.route('/')
