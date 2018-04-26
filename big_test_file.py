a8361e7e4de8af692c09c4931132fb43d6281512
Yaminn <yaminn.aung@hotmail.com>
2018-04-27 01:10:29 +1000

    altered test.py to add more tests

--- big_test_file.py
+++ big_test_file.py
@@ -1,4 +1,5 @@
 import subprocess
 subprocess.call("python neg_test.py qt3.14 1", shell=True)
 subprocess.call("python neg_test.py moose 1", shell=True)
+subprocess.call("python test.py", shell=True)
 
--- test.py
+++ test.py
@@ -13,7 +13,7 @@ def make_request(group_data, comp_name, start_date, end_date, stats):
         start_date = start_date.replace("Z","")
         end_date = end_date.replace("Z","")
     request = group_data["format"] %(comp_name, start_date, end_date, ",".join(stats))
-    print("================== currently testing %s =================" % group_data["name"])
+    print("================== currently testing %s for %s from %s to %s =================" % (group_data["name"], comp_name, start_date, end_date))
     try:
         webURL = urllib.request.urlopen(request)
         encoding = webURL.info().get_content_charset('utf-8')
@@ -29,7 +29,7 @@ def compare(group_data1, group_data2, comp_name, start_date, end_date, stats):
     res1 = json.dumps(make_request(group_data1, comp_name, start_date, end_date, stats),sort_keys=True, indent=4)
     res2 = json.dumps(make_request(group_data2, comp_name, start_date, end_date, stats),sort_keys=True, indent=4)
     diff = difflib.HtmlDiff().make_file(res1.splitlines(), res2.splitlines(), fromdesc=group_data1["name"], todesc=group_data2["name"])
-    with open ("comparing_%s_%s.html" % (group_data1["name"], group_data2["name"]), "w") as output_file:
+    with open ("comparing_%s_%s_%s.html" % (group_data1["name"], group_data2["name"], comp_name), "w") as output_file:
         output_file.write(diff)
 
 # Given a list of group data for different teams and some request parameters, compare 
@@ -47,3 +47,7 @@ api_list = [ { "name": "qt3.14", "format": "http://qt314.herokuapp.com/v2/compan
 # { "name": "crushing beasts", "format": "http://188.166.238.3/api/v1/fbstats?instid=%s&startdate=%s&enddate=%s&stats=%s", "time_format": "no_z" }  ]
 
 compare_all(api_list, "woolworths", "2015-10-01T08:45:10Z", "2015-11-01T08:45:10Z", ["id","name","website","description","category","fan_count","post_like_count","post_comment_count","post_type"])
+compare_all(api_list, "anzaustralia", "2016-11-01T08:45:10Z", "2017-12-01T08:45:10Z", ["id","name","website","description","category","fan_count"])
+compare_all(api_list, "accenture", "2017-11-01T00:00:00Z", "2017-11-09T00:00:00Z", ["id","name","website","description","category","fan_count", "post_type", "post_like_count", "post_comment_count"])
+compare_all(api_list, "HyundaiAustralia", "2017-03-01T00:00:00Z", "2017-11-09T12:59:00Z", ["id","name","website","description","category", "post_type", "post_like_count", "post_message"])
+compare_all(api_list, "medibank", "2018-01-12T00:00:00Z", "2018-02-12T00:00:00Z", ["id","name", "post_like_count", "post_message"])
