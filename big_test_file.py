e6240fecb861759a2b8501f191f11911987b2039
Yaminn <yaminn.aung@hotmail.com>
2018-04-27 01:13:00 +1000

    removed lines

--- big_test_file.py
+++ big_test_file.py
@@ -7,5 +7,4 @@ print("================= currently testing qt3.14 =================")
 subprocess.call("python neg_test.py qt3.14 1", shell=True)
 print("================= currently testing moose ==================")
 subprocess.call("python neg_test.py moose 1", shell=True)
-subprocess.call("python test.py", shell=True)
 
