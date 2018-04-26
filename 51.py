921b5c8ce30e7e21d5bf2598a31b371ba990cdef
sippycup <christinetvbui@gmail.com>
2018-04-27 01:46:59 +1000

    add 51.py

---
+++ 51.py
@@ -0,0 +1,50 @@
+import sys, requests, json
+
+api_list = {    "qt3.14": { "format":"http://qt314.herokuapp.com/v2/company/%s%s%s%s",
+                             "page":"accenture?",
+                             "start_date":"start_date=2015-10-01T08:45:00Z",
+                             "end_date": "&end_date=2016-10-01T08:45:00Z",
+                             "stats": "&stats=id"
+                         },
+                 "moose": {  "format": "http://seng3011-moose.herokuapp.com/v2/company%s%s%s%s",
+                             "page": "?id=accenture",
+                             "start_date": "&start_date=2015-10-01T08:45:00.000Z",
+                             "end_date": "&end_date=2015-10-01T08:45:00.000Z",
+                             "stats": "&stats=id"
+                         }
+             }
+
+
+def bad_format():
+    api = sys.argv[1]
+    req_deets = api_list[api]
+
+    # testing with badly formatted start and end dates
+    request = req_deets["format"] % (req_deets["page"], "&start_date=2015.10.01T", "&end_date=2016.10.01T", req_deets["stats"])
+    t1 = mk_req(request)
+
+    # testing with stats as list given with spaces instead of commas
+    request = req_deets["format"] % (req_deets["page"], req_deets["start_date"], req_deets["end_date"], "&stats=id name website")
+    t2 = mk_req(request)    
+    
+    if not t1 and not t2:
+        print("!error not returned upon incorrect formatting of query parameters")
+
+
+def mk_req(request):
+     res = requests.get(request)
+     print(res)
+     if str(res) != "<Response [400]>":
+         return True
+     try:
+         res_json = res.json()
+         if re.search('request_status.*:.*fail', str(res_json)):
+             return True
+     except:
+         pass
+     return False
+
+
+
+if __name__ == '__main__':
+    bad_format()
--- big_test_file.py
+++ big_test_file.py
@@ -4,7 +4,13 @@ subprocess.call("python3 test.py", shell=True)
 
 print("\n\n<<<<<<<<<< TEST 5.0 | testing missing parameters >>>>>>>>>>")
 print("================= currently testing qt3.14 =================")
-subprocess.call("python neg_test.py qt3.14 1", shell=True)
+subprocess.call("python 50.py qt3.14 1", shell=True)
 print("================= currently testing moose ==================")
-subprocess.call("python neg_test.py moose 1", shell=True)
+subprocess.call("python 50.py moose 1", shell=True)
 
+
+print("\n\n<<<<<<<<<< TEST 5.0 | testing incorrect parameter format >>>>>>>>>>")
+print("================= currently testing qt3.14 =================")
+subprocess.call("python 51.py qt3.14", shell=True)
+print("================= currently testing moose ==================")
+subprocess.call("python 51.py moose", shell=True)
